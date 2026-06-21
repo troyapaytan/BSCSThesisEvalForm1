6fdc621a5e8f05de34426be5025ce6cbc351eeb4
troyapaytan <troyandreipaytan@gmail.com>
2026-06-21 16:56:30 +0800

    vite config

--- eval-form/vite.config.ts
+++ eval-form/vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ADD THIS LINE:
+  base: '/projects/thesiseval1/',
 })
